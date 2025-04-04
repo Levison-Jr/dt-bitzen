a39be2686e1ac986ea2f411fba77895cf53da4f4
levisonjr_servlet <levisonjr21@gmail.com>
2025-04-04 11:51:25 -0300

    feat: Metodos para CRUD de usuarios

--- src/Services/Interfaces/IUsuarioService.cs
+++ src/Services/Interfaces/IUsuarioService.cs
@@ -1,6 +1,11 @@
-﻿namespace DTBitzen.Services.Interfaces
+﻿using DTBitzen.Models;
+
+namespace DTBitzen.Services.Interfaces
 {
     public interface IUsuarioService
     {
+        Task<bool> Editar(string id, Usuario usuario);
+
+        Task<bool> Excluir(string id);
     }
 }
