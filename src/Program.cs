6a8a56c17e5c3baae0ffc124073c4e610ce46376
levisonjr <levisonjr21@gmail.com>
2025-04-03 22:01:34 -0300

    feat: Condig de injeção de dependencias - jwt options - banco de dados postgres

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,15 @@
 
+using DTBitzen.Context;
+using DTBitzen.Identity;
+using DTBitzen.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
 namespace DTBitzen
 {
     public class Program
@@ -11,6 +22,81 @@ namespace DTBitzen
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDefaultIdentity<Usuario>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            var securityKeySettings = builder.Configuration.GetSection("JwtOptions:SecurityKey").Value;
+
+            if (string.IsNullOrEmpty(securityKeySettings))
+            {
+                throw new SecurityTokenException("JWT Security Key desconfigurada.");
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKeySettings));
+
+            builder.Services.Configure<JwtOptions>(options =>
+            {
+                builder.Configuration.GetSection(nameof(JwtOptions)).Bind(options);
+                options.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration.GetSection("JwtOptions:Issuer").Value,
+
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration.GetSection("JwtOptions:Audience").Value,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = securityKey,
+
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            builder.Services.AddAuthorization();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                options.User.RequireUniqueEmail = true;
+            });
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                string connectionString = builder.Configuration.GetConnectionString("DbConnection") ??
+                    throw new InvalidOperationException("Connection string indefinida.");
+
+                options.UseNpgsql(connectionString);
+            });
+
+            builder.Services.AddScoped<IIdentityHelper, IdentityHelper>();
+            builder.Services.AddScoped<AspNetUserManager<Usuario>>();
+            builder.Services.AddScoped<SignInManager<Usuario>>();
+
+            builder.Services.AddAutoMapper(typeof(Program));
 
             var app = builder.Build();
 
