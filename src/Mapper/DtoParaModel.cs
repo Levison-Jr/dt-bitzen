b974b25a74555a5348b8186fb740e754b97cd410
levisonjr <levisonjr21@gmail.com>
2025-04-04 14:45:31 -0300

    feat: Mapper Dto para Model

---
+++ src/Mapper/DtoParaModel.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DTBitzen.Dtos;
+using DTBitzen.Models;
+
+namespace DTBitzen.Mapper
+{
+    public class DtoParaModel : Profile
+    {
+        public DtoParaModel()
+        {
+            CreateMap<UsuarioDto, Usuario>();
+        }
+    }
+}
