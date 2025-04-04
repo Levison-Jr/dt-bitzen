abd11328e63ffaa42851633258aa2df32e95a526
levisonjr <levisonjr21@gmail.com>
2025-04-04 14:47:43 -0300

    feat: Implementada operações do Usuario

--- src/Services/Interfaces/IUsuarioService.cs
+++ src/Services/Interfaces/IUsuarioService.cs
@@ -4,6 +4,8 @@ namespace DTBitzen.Services.Interfaces
 {
     public interface IUsuarioService
     {
+        Task<Usuario?> BuscarPorId(string id);
+
         Task<bool> Editar(string id, Usuario usuario);
 
         Task<bool> Excluir(string id);
---
+++ src/Services/UsuarioService.cs
@@ -0,0 +1,47 @@
+﻿using DTBitzen.Models;
+using DTBitzen.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace DTBitzen.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly AspNetUserManager<Usuario> _aspNetUserManager;
+
+        public UsuarioService(AspNetUserManager<Usuario> aspNetUserManager)
+        {
+            _aspNetUserManager = aspNetUserManager;
+        }
+
+        public async Task<Usuario?> BuscarPorId(string id)
+        {
+            return await _aspNetUserManager.FindByIdAsync(id);
+        }
+
+        public async Task<bool> Editar(string id, Usuario usuario)
+        {
+            Usuario? usuarioParaEditar = await _aspNetUserManager.FindByIdAsync(id);
+
+            if (usuarioParaEditar is null)
+                return false;
+
+            usuarioParaEditar.Nome = usuario.Nome;
+
+            var resultado = await _aspNetUserManager.UpdateAsync(usuarioParaEditar);
+
+            return resultado.Succeeded;
+        }
+
+        public async Task<bool> Excluir(string id)
+        {
+            Usuario? usuarioParaExcluir = await _aspNetUserManager.FindByIdAsync(id);
+
+            if (usuarioParaExcluir is null)
+                return false;
+
+            var resultado = await _aspNetUserManager.DeleteAsync(usuarioParaExcluir);
+
+            return resultado.Succeeded;
+        }
+    }
+}
